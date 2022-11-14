e85fe67b3dad120601179565fff8bcb07ebbb58f
Gary <garykpdx145@gmail.com>
2022-11-14 15:43:43 -0600

    streamlining get_daily_averages

--- task/utils.py
+++ task/utils.py
@@ -1,10 +1,4 @@
-def should_exclude(price, count):
-    # separate exclusion logic from averaging
-    return count >= 3
-
-
 def get_daily_averages(prices):
-    # [(date(2016, 1, 1), 1383), (date(2016, 1, 2), 1383)]
     daily_prices = {}
     daily_counts = {}
     for day, price in prices:
@@ -12,9 +6,8 @@ def get_daily_averages(prices):
         daily_counts[day] = daily_counts.get(day, 0) + 1
 
     for day in daily_prices:
-        if should_exclude(daily_prices[day], daily_counts[day]):
+        if daily_counts[day] < 3:
             daily_prices[day] = None
         else:
             daily_prices[day] = daily_prices[day] / daily_counts[day]
-
-    return daily_prices
+    return list(daily_prices.items())
