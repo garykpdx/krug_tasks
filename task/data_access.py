6294a6d6dec1e6d1f17811ced56dc8963a336a62
Gary <garykpdx145@gmail.com>
2022-11-14 17:02:30 -0600

    reformatting

--- task/data_access.py
+++ task/data_access.py
@@ -49,7 +49,7 @@ class DataAccess:
         cursor.execute(sql, (region,))
         return [value[0] for value in cursor.fetchall()]
 
-    def get_ports_from_regions(self, regions:list) -> list:
+    def get_ports_from_regions(self, regions: list) -> list:
         cursor = self.con.cursor()
         sql_list = "({})".format(','.join(["'%s'" % r for r in regions]))
         sql = "SELECT code FROM ports WHERE parent_slug in %s" % sql_list
@@ -57,19 +57,20 @@ class DataAccess:
 
         return [value[0] for value in cursor.fetchall()]
 
-    def get_prices(self, orig_ports:list, dest_ports:list, date_from:date, date_to:date) -> list:
+    def get_prices(self, orig_ports: list, dest_ports: list, date_from: date, date_to: date) -> list:
         cursor = self.con.cursor()
         orig_ports_list = "({})".format(','.join(["'%s'" % p for p in orig_ports]))
         dest_ports_list = "({})".format(','.join(["'%s'" % p for p in dest_ports]))
         cursor.execute(f"SELECT day, price FROM prices WHERE orig_code in"
                        f" {orig_ports_list} and dest_code in {dest_ports_list} "
-                       "and day >= %s and day <= %s", ( date_from, date_to))
-        return [(day.strftime('%Y-%m-%d'),price) for day,price in cursor.fetchall()]
+                       "and day >= %s and day <= %s", (date_from, date_to))
+        return [(day.strftime('%Y-%m-%d'), price) for day, price in cursor.fetchall()]
 
     def get_results(self, origin: str, dest: str, date_from: date, date_to: date) -> list:
-        origin_list:list = self.get_ports_from_regions(self.get_all_subregions(origin)) if is_region(origin) else [origin]
-        dest_list:list = self.get_ports_from_regions(self.get_all_subregions(dest)) if is_region(dest) else [dest]
-        prices:list = self.get_prices(origin_list, dest_list, date_from, date_to)
+        origin_list: list = self.get_ports_from_regions(self.get_all_subregions(origin)) if is_region(origin) else [
+            origin]
+        dest_list: list = self.get_ports_from_regions(self.get_all_subregions(dest)) if is_region(dest) else [dest]
+        prices: list = self.get_prices(origin_list, dest_list, date_from, date_to)
         return get_daily_averages(prices)
 
     def close(self):
