6d997ce05505cf9a300b560bbce00507c06dc7fb
Gary <garykpdx145@gmail.com>
2022-11-14 09:20:02 -0600

    add dummy return value

--- task/app.py
+++ task/app.py
@@ -1,24 +1,27 @@
 from flask import Flask
 from flask import request
 
+from task.data_access import DataAccess
+
 app = Flask(__name__)
+
 
 @app.route('/rates', methods=['GET'])
 def get_average_rate():
-    # date_from
-    # date_to
-    # origin
-    # destination
-
     date_from = request.args.get('date_from')
     date_to = request.args.get('date_to')
     origin = request.args.get('origin')
     destination = request.args.get('destination')
-    return {'origin': origin,'destination': destination, 'date_from': date_from, 'date_to': date_to}
+    data = DataAccess()
+    results = data.get_results(origin, destination, date_from, date_to)
+    return results
 
-@app.route('/status', methods=['POST', 'GET'])
+
+@app.route('/status', methods=['GET', 'POST'])
 def get_status():
-    return {'status': 'running'}
+    data = DataAccess()
+    version = data.get_version()
+    return {'status': 'running', 'database': version}
 
 
 if __name__ == '__main__':
