9ca0bb49010b8a2d60f63f8f07fdcbc8463ea5df
Gary <garykpdx145@gmail.com>
2022-11-14 19:26:30 -0600

    adding exception handling for bad parameters

--- task/app.py
+++ task/app.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime
 
 from flask import Flask
+from flask import abort
 from flask import request
 
 from task.data_access import DataAccess
@@ -10,10 +11,17 @@ app = Flask(__name__)
 
 @app.route('/rates', methods=['GET'])
 def get_average_rate():
-    date_from = datetime.strptime(request.args.get('date_from'), "%Y-%m-%d").date()
-    date_to = datetime.strptime(request.args.get('date_to'), "%Y-%m-%d").date()
+    try:
+        date_from = datetime.strptime(request.args.get('date_from'), "%Y-%m-%d").date()
+        date_to = datetime.strptime(request.args.get('date_to'), "%Y-%m-%d").date()
+    except TypeError:
+        abort(400)
+
     origin = request.args.get('origin')
     destination = request.args.get('destination')
+    
+    if not origin or not destination:
+        abort(400)
 
     data = DataAccess()
     results = data.get_results(origin, destination, date_from, date_to)
